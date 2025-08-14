889d78b2f4e9ef650c1b08675a5a9841893a6e0f
boral <boral@shripad295>
2025-08-14 17:21:15 +0530

    new case added

--- src/service/TeamService.java
+++ src/service/TeamService.java
@@ -44,6 +44,45 @@ public class TeamService {
 			getTeamByShortName(teamShortName);
 			break;
 		}
+		case 3:{
+			System.out.println("Enter Your Team ID : ");
+			int id = sc.nextInt();
+			
+			if(id==101) {
+				System.out.println(TeamRepository.getMITeamDetails());
+			}
+			else if(id==102) {
+				System.out.println(TeamRepository.getRCBTeamDetails());
+			}
+			else if(id==103) {
+				System.out.println(TeamRepository.getCSKTeamDetails());
+			}
+			else if(id==104) {
+				System.out.println(TeamRepository.getRRTeamDetails());
+			}
+			else if(id==105) {
+				System.out.println(TeamRepository.getDCTeamDetails());
+			}
+			else if(id==106) {
+				System.out.println(TeamRepository.getGTTeamDetails());
+			}
+			else if(id==107) {
+				System.out.println(TeamRepository.getSRHTeamDetails());
+			}
+			else if(id==108) {
+				System.out.println(TeamRepository.getKKRTeamDetails());
+			}
+			else if(id==109) {
+				System.out.println(TeamRepository.getPBKSTeamDetails());
+			}
+			else if(id==110) {
+				System.out.println(TeamRepository.getLSGTeamDetails());
+			}
+			else {
+				System.out.println("No Team found with id : " + id);
+			}
+			break;
+		}
 		
 		default:
 			throw new IllegalArgumentException("Unexpected value: " + input);
